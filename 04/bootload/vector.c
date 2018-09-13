b5546c7b794aa7f1bb32faf05d12c7be58dbf43b
ei <chronolavos0124@gmail.com>
2018-09-13 09:49:14 +0900

    Add xmodem.h and .c

---
+++ 04/bootload/vector.c
@@ -0,0 +1,14 @@
+#include "defines.h"
+
+extern void start(void);
+
+void (*vectors[])(void) = { //set pointer to start()
+    start, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+    NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL,
+};
