27a9ceb1eba40084dc25113aba759e570be2a765
ei <chronolavos0124@gmail.com>
2018-10-09 20:31:39 +0900

    add kozos

--- 10/os/kozos.c
+++ 10/os/kozos.c
@@ -3,6 +3,7 @@
 #include "intr.h"
 #include "interrupt.h"
 #include "syscall.h"
+#include "memory.h"
 #include "lib.h"
 
 #define THREAD_NUM 6 
@@ -221,6 +222,21 @@ static int thread_chpri(int priority)
 	return old;
 }
 
+// get dynamic memory
+static void *thread_kmalloc(int size)
+{
+	putcurrent();
+	return kzmem_alloc(size);
+}
+
+// free memory
+static int thread_kmfree(char *p)
+{
+	kzmem_free(p);
+	putcurrent();
+	return 0;
+}
+
 // regist interrupt handler
 static int setintr(softvec_type_t type, kz_handler_t handler)
 {
@@ -258,6 +274,12 @@ static void call_functions(kz_syscall_type_t type, kz_syscall_param_t *p)
 			break;
 		case KZ_SYSCALL_TYPE_CHPRI:
 			p->un.chpri.ret = thread_chpri(p->un.chpri.priority);
+			break;
+		case KZ_SYSCALL_TYPE_KMALLOC:
+			p->un.kmalloc.ret = thread_kmalloc(p->un.kmalloc.size);
+			break;
+		case KZ_SYSCALL_TYPE_KMFREE:
+			p->un.kmfree.ret = thread_kmfree(p->un.kmfree.p);
 			break;
 		default:
 			break;
@@ -313,6 +335,8 @@ static void thread_intr(softvec_type_t type, unsigned long sp)
 
 void kz_start(kz_func_t func, char *name, int priority, int stacksize, int argc, char *argv[])
 {
+	kzmem_init();
+
 	current = NULL;
 	
 	// init
